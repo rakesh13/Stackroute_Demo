b85b7feead766d451f07b87d313de7bc80baf651
Rakesh Kumar Pandit <Rakesh.Pandit@in.niit.com>
2024-06-12 18:54:09 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/home'
+import About from './components/about';
+import Contact from './components/contact';
+
+
+function App() {
+  return (
+        <Router>
+          <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </div>
+        </Router>
+      );
+}
+
+export default App;
---
+++ src/components/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Contact()
+{
+    return (
+        <div>
+            <h2>Contact Us Page</h2>
+            <p>You can use this page to reach out to us!</p>
+        </div>
+    );
+}
